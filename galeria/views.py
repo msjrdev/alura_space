a9122f4e53782fdb73338848f36e2d34d31c144e
Manoel Silva Junior <msilvajrdev@gmail.com>
2024-11-21 17:53:00 -0300

    Foi criado um superuser no terminal para acessar o Django Admin

--- galeria/views.py
+++ galeria/views.py
@@ -1,17 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-
+from galeria.models import Fotografia
 
 def index(request):
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
-    dados = {
-        1: {"nome": "Nebulosa de Carina",
-            "legenda": "webbtelecope.org / NASA / James Webb"},
-        2: {"nome": "Galáxia NGC 1079",
-            "legenda": "nasa.org / NASA / Hubble"},
-    }
-
-    return render(request, 'galeria/index.html', {"cards": dados})
-
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
